e27c2f8f4b980e00efdd90926bbf3bee19f68de2
Tam CARRE <kokone@takat.su>
2021-12-06 06:29:20 +0100

    ViewPatterns

--- src/Day2/Input.hs
+++ src/Day2/Input.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE ViewPatterns #-}
+
 module Day2.Input
   ( input
   , Command (..)
@@ -12,10 +14,7 @@ input :: IO [Command]
 input = map toCommand . lines <$> readFile "./src/Day2/input.txt"
 
 toCommand :: String -> Command
-toCommand str =
-  let cmdWords = words str
-  in case (head cmdWords, read $ cmdWords !! 1) of
-       ("forward", n) -> Fwd n
-       ("down", n)    -> Down n
-       ("up", n)      -> Up n
-       (_, _)         -> Up 0
+toCommand (words -> [verb, n]) = case (verb, read n) of
+  ("forward", n) -> Fwd n
+  ("down", n)    -> Down n
+  ("up", n)      -> Up n
